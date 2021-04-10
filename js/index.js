9412eb2301a44c41620d53fec651bc10750fb2b6
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-04-11 01:37:54 +0300

    completed project

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    var austDay = new Date();
+    austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+    $("#defaultCountdown").countdown({ until: austDay, format: 'odHMS' });
+
+    var wow = new WOW({ mobile: false });
+    wow.init();
+
+    $('.form-control').blur(function () {
+        var x = document.forms["myform"]["email"].value;
+        var atpos = x.indexOf("@");
+        var dotpos = x.lastIndexOf(".");
+
+        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+            $(this).parent().find(".alert.alert-danger").fadeIn(200);
+        } else {
+            $(this).parent().find(".alert.alert-success").fadeIn(200);
+        }
+    });
+    $('.form-control').blur(function () {
+        var x = document.forms["myform2"]["email2"].value;
+        var atpos = x.indexOf("@");
+        var dotpos = x.lastIndexOf(".");
+        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+            $(this).parent().find(".alert.alert-danger").fadeIn(200);
+        } else {
+            $(this).parent().find(".alert.alert-success").fadeIn(200);
+        }
+    });
+});
